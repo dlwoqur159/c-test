56cb846404980cc69ef47a30b1e93ca5dce0acc1
user <user>
2022-10-28 15:16:31 +0900

    git 첫시도

--- c++test/main.cpp
+++ c++test/main.cpp
@@ -3,9 +3,15 @@
 
 int main()
 {
-	//int : 자료형
+	/* 자료형 (크기 단위 : byte)
+	정수형 : char(1), short(2), int(4), long(4), long long(8)
+	실수형 : float(4), double(8)
+	*/
+
+	 
 	int i = 0;
 
+	short data = 0;
 
 
 	return 0;
